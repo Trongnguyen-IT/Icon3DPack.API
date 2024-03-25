c2d72c03fc8111bed37d7169ff5372109d60f947
Trongnguyen-IT <nguyennhattrong@gmail.com>
2024-03-25 15:44:24 +0700

    add method type

--- src/Icon3DPack.API.Host/Controllers/HealthController.cs
+++ src/Icon3DPack.API.Host/Controllers/HealthController.cs
@@ -4,6 +4,7 @@ namespace Icon3DPack.API.Host.Controllers
 {
     public class HealthController : ApiController
     {
+        [HttpGet]
         public IActionResult Health()
         {
             return Ok(new { messsage = "ok" });
