befc2dc9ba87291154d0f928f9dd5d461aa50282
Trongnguyen-IT <nguyennhattrong@gmail.com>
2024-03-25 15:42:10 +0700

    health check

---
+++ src/Icon3DPack.API.Host/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Icon3DPack.API.Host.Controllers
+{
+    public class HealthController : ApiController
+    {
+        public IActionResult Health()
+        {
+            return Ok(new { messsage = "ok" });
+        }
+    }
+}
